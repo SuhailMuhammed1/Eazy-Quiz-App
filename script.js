0ce2196df329559ba716e6d0b60d4f5d5d762005
SuhailMuhammed1 <suhailjr111@gmail.com>
2024-04-29 12:34:35 +0530

    API Added

--- script.js
+++ script.js
@@ -4,21 +4,24 @@ let timer;
 let questions;
 let score = 0;
 
-fetch('questions.json')
+fetch('https://opentdb.com/api.php?amount=5&category=21&type=multiple')
     .then(response => response.json())
     .then(data => {
-        questions = data.questions;
+        questions = data.results;
         loadQuestion(questions[currentQuestion]);
     })
     .catch(error => console.error('Error loading questions:', error));
 
 function loadQuestion(question) {
-    document.getElementById('question').innerText = question.text;
+    document.getElementById('question').innerText = `${currentQuestion + 1}. ${question.question}`;
 
     const optionsContainer = document.getElementById('options');
     optionsContainer.innerHTML = '';
 
-    question.options.forEach(option => {
+    const allOptions = [...question.incorrect_answers, question.correct_answer];
+    shuffleArray(allOptions);
+
+    allOptions.forEach(option => {
         const optionDiv = document.createElement('div');
         optionDiv.textContent = option;
         optionDiv.classList.add('option');
@@ -29,22 +32,6 @@ function loadQuestion(question) {
     startTimer();
 }
 
-// function selectOption(selectedOption, question) {
-//     const options = document.querySelectorAll('.option');
-//     options.forEach(option => {
-//         if (option === selectedOption) {
-//             option.classList.add('selected');
-//             if (selectedOption.textContent === question.answer) {
-//                 option.style.background="green";
-//                 option.style.color="white";
-//                 score++;
-//             }
-//         } else {
-//             option.classList.remove('selected');
-//         }
-//     });
-// }
-
 function selectOption(selectedOption, question) {
     const options = document.querySelectorAll('.option');
     if (document.querySelector('.option.selected')) {
@@ -53,17 +40,20 @@ function selectOption(selectedOption, question) {
     options.forEach(option => {
         if (option === selectedOption) {
             option.classList.add('selected');
-            if (selectedOption.textContent === question.answer) {
+            if (selectedOption.textContent === question.correct_answer) {
                 option.style.background = "green";
                 option.style.color = "white";
+                option.innerHTML += '<i class="fas fa-check"></i>';
                 score++;
             } else {
                 option.style.background = "red";
+                option.innerHTML += '<i class="fas fa-times"></i>';
             }
         } else {
-            if (option.textContent === question.answer) {
+            if (option.textContent === question.correct_answer) {
                 option.style.background = "green";
                 option.style.color = "white";
+                option.innerHTML += '<i class="fas fa-check"></i>';
             } else {
                 option.style.background = "";
             }
@@ -72,36 +62,26 @@ function selectOption(selectedOption, question) {
     });
 }
 
-
-
 function startTimer() {
     timeLeft = 15;
     clearInterval(timer);
     timer = setInterval(() => {
-        if (timeLeft <= 0) {
+        if (timeLeft == 0) {
             clearInterval(timer);
             goToNextQuestion();
         } else {
-            document.getElementById('timer').textContent = `Time Left: ${timeLeft} seconds`;
+            document.getElementById('timer').textContent = `Time Left: ${timeLeft}`;
+            document.getElementById('line').style.width = `${(15 - timeLeft) / 15 * 110}%`;
             timeLeft--;
         }
     }, 1000);
 }
 
-// function submitAnswer() {
-//     const selectedOption = document.querySelector('.option.selected');
-//     if (!selectedOption && timeLeft > 0) {
-//         alert('Please select an option before submitting.');
-//         return; 
-//     }
-//     goToNextQuestion();
-// }
-
 function goToNextQuestion() {
     const selectedOption = document.querySelector('.option.selected');
     if (!selectedOption && timeLeft > 0) {
         alert('Please select an option before submitting.');
-        return; 
+        return;
     }
     clearInterval(timer);
     currentQuestion++;
@@ -113,6 +93,7 @@ function goToNextQuestion() {
 }
 
 function endQuiz() {
+    document.getElementById('line').style.display = 'none';
     const scoreDisplay = document.getElementById('question');
     scoreDisplay.innerHTML = `<center> Quiz Ended! <br> Your Score: ${score}/${questions.length} </center>`;
     document.getElementById('options').innerHTML = '';
@@ -121,10 +102,11 @@ function endQuiz() {
     btnSubmit.textContent = 'Restart';
     btnSubmit.removeEventListener('click', goToNextQuestion);
     btnSubmit.addEventListener('click', restartQuiz);
-    score = 0; 
+    score = 0;
 }
 
 function restartQuiz() {
+    document.getElementById('line').style.display = 'block';
     currentQuestion = 0;
     loadQuestion(questions[currentQuestion]);
     const btnSubmit = document.getElementById('btnsubmit');
@@ -134,3 +116,10 @@ function restartQuiz() {
 }
 
 document.getElementById('btnsubmit').addEventListener('click', goToNextQuestion);
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
